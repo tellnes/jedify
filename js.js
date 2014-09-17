3e3cd3111c3856c1e16245bdb8447d13a1228015
Niklas Närhinen <niklas@narhinen.net>
2014-09-17 22:06:29 +0300

    Allow passing `lang`  as option

--- js.js
+++ js.js
@@ -6,12 +6,14 @@ var through = require('through')
 
 var argv = minimist(process.argv.slice(2))
 
-var lang = argv['jedify-lang'] || process.env['JEDIFY_LANG'] || 'en'
+var defaultLang = argv['jedify-lang'] || process.env['JEDIFY_LANG'] || 'en'
 
 var re = /\.js$/
 
-module.exports = function (file) {
+module.exports = function (file, options) {
   if (!re.test(file)) return through()
+  options = options || {}
+  var lang = options.lang || defaultLang;
 
   var buf = []
     , stream = through(write, end)
