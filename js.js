a172dc9a3ee1343933140b97f8a2bca48c43cff6
Niklas Närhinen <niklas@narhinen.net>
2014-09-19 19:58:49 +0300

    Remove `--jedify-lang` command line option

--- js.js
+++ js.js
@@ -4,16 +4,14 @@ var through = require('through')
   , util = require('util')
   , minimist = require('minimist')
 
-var argv = minimist(process.argv.slice(2))
-
-var defaultLang = argv['jedify-lang'] || process.env['JEDIFY_LANG'] || 'en'
+var defaultLang = process.env['JEDIFY_LANG'] || 'en'
 
 var re = /\.js$/
 
 module.exports = function (file, options) {
   if (!re.test(file)) return through()
   options = options || {}
-  var lang = options.lang || defaultLang;
+  var lang = options.lang || defaultLang
 
   var buf = []
     , stream = through(write, end)
